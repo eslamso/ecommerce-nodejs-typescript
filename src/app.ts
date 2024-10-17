634ba8ceae13c6ec031527df4cc7582bf45cc97b
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-10-18 02:34:22 +0300

    woop

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/payTabsWebhook", payTabsWebHook);
+app.post("/payTabsWebhook", payTabsWebHook);
 //mount routes
 mountRoutes(app);
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
