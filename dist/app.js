34b6246728b52118e15071bcc1156c25b86f3843
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-10-19 19:01:48 +0300

    last thing done

--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const cors_1 = __importDefault(require("cors"));
+const compression_1 = __importDefault(require("compression"));
+const express_rate_limit_1 = require("express-rate-limit");
 const mountRoutes_1 = __importDefault(require("./controllers/mountRoutes"));
+const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
+const hpp_1 = __importDefault(require("hpp"));
 const error_middleWare_1 = require("./middlewares/error.middleWare");
 const order_services_1 = require("./services/order.services");
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.options("*", (0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.json());
+app.use((0, compression_1.default)());
+app.use(express_1.default.json({ limit: "20Kb" }));
 app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "uploads")));
 app.post("/payTabsWebhook", order_services_1.payTabsWebHook);
+const limiter = (0, express_rate_limit_1.rateLimit)({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+});
+app.use(limiter);
+app.use((0, express_mongo_sanitize_1.default)());
+app.use((0, hpp_1.default)());
 //mount routes
 (0, mountRoutes_1.default)(app);
 app.all("*", (req, res, next) => {
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
 import path from "path";
 import express, { Request, Response, NextFunction, Express } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+import { rateLimit } from "express-rate-limit";
 import mountRoutes from "./controllers/mountRoutes";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
 import { globalErrorHandler } from "./middlewares/error.middleWare";
 import { payTabsWebHook } from "./services/order.services";
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(compression());
+app.use(express.json({ limit: "20Kb" }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.post("/payTabsWebhook", payTabsWebHook);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes)
+});
+app.use(limiter);
+app.use(mongoSanitize());
+app.use(hpp());
 //mount routes
 mountRoutes(app);
 app.all("*", (req: Request, res: Response, next: NextFunction) => {
